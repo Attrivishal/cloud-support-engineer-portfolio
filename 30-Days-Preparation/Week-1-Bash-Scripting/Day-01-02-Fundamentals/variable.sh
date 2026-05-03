f0455c2596932d483d61d92397c27f2f68650a06
Vishal Attri <vishalattri196@gmail.com>
2026-05-03 15:31:52 +0530

    Added new script file

---
+++ 30-Days-Preparation/Week-1-Bash-Scripting/Day-01-02-Fundamentals/variable.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# This script demonstrates the use of variables in bash scripting.
+# always use double quotes when referencing variables
+
+now=$(date)
+echo "Current date and time: $now"
+Name="vishal"
+age=21
+University="LPU"
+Course="BCA"
+
+echo "My name is $Name, I am $age years old , and I am currently pursuing $Course from $University , thank you for asking :)"
+
+#why we exactly use variable in production environment?
+
+#1. Reusability: Variables allow you to reuse values throughout your script without hardcoding them multiple times. This makes your code more efficient and easier to maintain.
