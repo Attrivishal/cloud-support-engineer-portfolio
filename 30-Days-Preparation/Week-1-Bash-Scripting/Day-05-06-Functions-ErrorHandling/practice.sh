8d459c29dd8746146ad41a1b6e94d1b43e948d1e
Vishal Attri <vishalattri196@gmail.com>
2026-05-18 22:40:03 +0530

    New changes done.

---
+++ 30-Days-Preparation/Week-1-Bash-Scripting/Day-05-06-Functions-ErrorHandling/practice.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+## Creating first function 
+
+greet(){
+    echo "Hello i am vishal attri."
+}
+
+greet
+
+
+# system-info() {
+#  echo "Memory is:-"
+#  df -h
+#  echo "------------"
+
+#  echo "Free memory is:-"
+#  free -h
+#  echo "------------"
+
+#  echo "disk usage is:-"
+#  du -sh
+# }
+
+# system-info
+
+#Function with arguments
+
+greet_person() {
+    echo "Hello $1,How are you doing?"
+}
+
+greet_person "Vishal Attri"
+greet_person "Khushi"
