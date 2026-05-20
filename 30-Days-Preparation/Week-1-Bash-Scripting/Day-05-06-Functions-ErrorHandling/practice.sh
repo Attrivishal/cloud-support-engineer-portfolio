c89168e9fdbe2ad97e37b6b8d871e45b787d14c8
Vishal Attri <vishalattri196@gmail.com>
2026-05-20 22:17:08 +0530

    New changes done

--- 30-Days-Preparation/Week-1-Bash-Scripting/Day-05-06-Functions-ErrorHandling/practice.sh
+++ 30-Days-Preparation/Week-1-Bash-Scripting/Day-05-06-Functions-ErrorHandling/practice.sh
@@ -32,3 +32,31 @@ greet_person() {
 greet_person "Vishal Attri"
 greet_person "Khushi"
 
+#checking disk usage and free memory
+mem_usage() {
+    usage=$(du -sh / | awk 'NR==2  {print $5}' | sed 's/%//')
+    echo "$usage"
+}
+
+mem_usages=$(mem_usage)
+echo "Your disk is: $(mem_usages)% full"
+
+
+#check the number if even or odd if even return 0 else return 1
+is_even(){
+    if [ $(( $1 % 2 )) -eq 0 ];then
+    return 0
+    else
+    return 1
+    fi
+}
+#function calling 
+is_even 4
+if [ $? -eq 0 ];
+then
+     echo "Number is even"
+else
+    echo "Number is odd"
+fi
+
+# get file size
